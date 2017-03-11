22fb926109653c5512773a70d1a1d36c2718ce1c
yuyasat <takiyour@gmail.com>
2017-03-11 23:45:17 +0900

    update plan table

--- db/migrate/20170219000000_create_plans.rb
+++ db/migrate/20170219000000_create_plans.rb
@@ -8,9 +8,9 @@ class CreatePlans < ActiveRecord::Migration[5.0]
       t.integer :sms_num,         null: false, default: 0
       t.integer :voice_num,       null: false, default: 0
       t.integer :price,           null: false
-      t.string :down_speed,       null: false, default: ""
-      t.string :up_speed,         null: false, default: ""
-      t.string :voice_price,      null: false, default: ""
+      t.string :down_speed,       null: true
+      t.string :up_speed,         null: true
+      t.string :voice_price,      null: true
       t.string :sms_domestic
       t.string :sms_international
       t.boolean :carry_over,      null: false, default: false
