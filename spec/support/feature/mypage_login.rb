512f3cdc1742de4da067cfbb7d6ea90836d21f8a
yuyasat <takiyour@gmail.com>
2017-04-09 12:36:43 +0900

    delete unnecessary spec files

--- spec/support/feature/mypage_login.rb
+++
@@ -1,5 +0,0 @@
-RSpec.shared_context 'my page login in feature' do
-  let(:user) { create(:user) }
-
-  before { login_as user }
-end
--- spec/support/mypage_login.rb
+++
@@ -1,8 +0,0 @@
-RSpec.shared_context 'mypage login' do
-  let(:user) { create(:user) }
-
-  before do
-    @request.env['devise.mapping'] = Devise.mappings[:user]
-    sign_in user
-  end
-end
